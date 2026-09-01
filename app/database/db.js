3677ca61300fdc342fd3e33ef9a2eb860980d80b
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-01 14:41:06 +0100

    create file controler, route and db

---
+++ app/database/db.js
@@ -0,0 +1,11 @@
+import pg from "pg";
+
+const { Client } = pg;
+
+export const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 import express from "express";
-import pg from "pg";
-
-const { Client } = pg;
-
+import { client } from "./app/database/db.js";
 const app = express();
 
 const port = process.env.PORT || 3000;
 console.log("process.env.PORT : ", process.env.PORT);
-
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
 
 async function startServer() {
   try {

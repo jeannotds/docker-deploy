e396f01dcd1788029ba029360754b30a2eef3b28
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-01 14:18:23 +0100

    connect database

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const express = require("express");
+import express from "express";
+import pg from "pg";
+
+const { Client } = pg;
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 console.log("process.env.PORT : ", process.env.PORT);
 
-app.get("/", (req, res) => {
-  res.send("Version 2 avec Docker Compose !");
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-app.listen(port, () => {
-  console.log(`Application démarrée sur le port ${port}`);
-});
+async function startServer() {
+  try {
+    await client.connect();
+
+    console.log("✅ Connecté à PostgreSQL !");
+
+    // Test de la connexion
+    const result = await client.query("SELECT NOW()");
+    console.log("📅 Heure PostgreSQL :", result.rows[0]);
+
+    app.get("/", (req, res) => {
+      res.send("Version 2 avec Docker Compose !");
+    });
+
+    app.listen(port, () => {
+      console.log(`Application démarrée sur le port ${port}`);
+    });
+  } catch (err) {
+    console.log("❌ Erreur de connexion à PostgreSQL : ", err.message);
+  }
+}
+
+await startServer();
